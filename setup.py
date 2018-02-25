a6358d3bffdb950f92fc10c45fdfdedd8dad053d
Sergei Kotlyachkov <kotl@google.com>
2018-02-25 17:54:59 -0500

    more urls modifications to point to current repo

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Jim DeVona"
 plugin_author_email = "jim@anoved.net"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/anoved/OctoPrint-EmailNotifier"
+plugin_url = "https://github.com/kotl/OctoPrint-EmailNotifier"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
